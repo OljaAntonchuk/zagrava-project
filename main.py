4b5d8a09bee73a2c905829c8ff1cc8a678b1ff46
Olha Antonchuk <antonchuk_ak20@nuwm.edu.ua>
2024-05-01 12:20:52 +0300

    commit

--- main.py
+++ main.py
@@ -1,60 +1,84 @@
-﻿from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
+﻿from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+from __main__ import db, app
 
-app = Flask('__name__')
-app.secret_key = 'asdfg'
+with app.app_context():
+    db.create_all()
 
-#MySQL Configuration
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask_users'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-mysql = MySQL(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
-@app.route('/')
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+posts = [
+    {
+        'author': 'Olha Antonchuk',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
 def home():
-    if 'username' in session:
-        return render_template('home.html', username=session['username'])
-    else:
-        return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        username = request.form['username']
-        pwd = request.form['password']
-        cur = mysql.connection.cursor()
-        cur.execute(f"select username, password from tbl_users where username =  '{username}'")
-        user = cur.fetchone()
-        cur.close()
-        if user and pwd == user[1]:
-            session['username'] = user[0]
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            return render_template('login.html', error='Invalid username or password')
-
-    return render_template('login.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == "POST":
-        username = request.form['username']
-        pwd = request.form['password']
-
-        cur = mysql.connection.cursor()
-        cur.execute(f"insert into tbl_users (username, password) values('{username}', '{pwd}')")
-        mysql.connection.commit()
-        cur.close()
-
-        return redirect(url_for('login'))
-    return render_template('register.html')
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('home'))
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
